f943613fde6878390642a9b290993ac3be117c90
Yash Agarwal <yashagar0412@gmail.com>
2021-06-27 23:03:34 +0530

    added git link and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/header/header";
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
       <Header />
+      <h2 className="text-center">More content to appear soon!</h2>
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,11 @@
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import MenuIcon from '@material-ui/icons/Menu';
-import React from "react";
+import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,9 +33,17 @@ export default function Header() {
           <MenuIcon />
         </IconButton>
         <Typography variant="h6" className={classes.title}>
-          News
+          Yash Agarwal
         </Typography>
-        <Button color="inherit">Login</Button>
+
+        <IconButton
+          color="inherit"
+          href="https://github.com/yash0412"
+          target="_blank"
+          aria-label="github link"
+        >
+          <GitHubIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
