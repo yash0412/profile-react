c950d64d8fbe5069d5d6953412ab1d50d88ca524
Yash Agarwal <yashagar0412@gmail.com>
2021-06-28 21:18:19 +0530

    added socials

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,8 +2,10 @@ import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
 
@@ -35,15 +37,26 @@ export default function Header() {
         <Typography variant="h6" className={classes.title}>
           Yash Agarwal
         </Typography>
-
-        <IconButton
-          color="inherit"
-          href="https://github.com/yash0412"
-          target="_blank"
-          aria-label="github link"
-        >
-          <GitHubIcon />
-        </IconButton>
+        <Tooltip title="LinkedIn Profile" aria-label="LinkedIn Profile">
+          <IconButton
+            color="inherit"
+            href="https://www.linkedin.com/in/yash-agarwal-a6a92a133/"
+            target="_blank"
+            aria-label="linkedin link"
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Github Profile" aria-label="Github Profile">
+          <IconButton
+            color="inherit"
+            href="https://github.com/yash0412"
+            target="_blank"
+            aria-label="github link"
+          >
+            <GitHubIcon />
+          </IconButton>
+        </Tooltip>
       </Toolbar>
     </AppBar>
   );
